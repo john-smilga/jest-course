d46f7ea039ee3120a6af38a8264176878fd7e935
Coding Addict <johnsmilga@gmail.com>
2025-04-11 10:47:28 +0300

    update 04-errors

--- 04-testing-errors/src/services/01-users/UserService.ts
+++ 04-testing-errors/src/services/01-users/UserService.ts
@@ -2,10 +2,6 @@
 
 import { NewsletterService } from './NewsletterService';
 import { DatabaseService } from './DatabaseService';
-import { AppCodes } from '../../utils/AppCodes';
-import { CustomError } from '../../utils/CustomError';
-import { HttpCodes } from '../../utils/HttpCodes';
-import { CustomLogger } from '../../utils/CustomLogger';
 
 export class UserService {
   constructor(private name: string, private email: string) {}
@@ -14,20 +10,10 @@ export class UserService {
     try {
       const user = await DatabaseService.createUser(this.name, this.email);
       await NewsletterService.subscribeUser(user);
-      CustomLogger.info(
-        'UserService registerUser',
-        AppCodes.REGISTER_USER_SUCCESS,
-        {
-          user,
-        }
-      );
+
       return { msg: 'user registered successfully' };
     } catch (error) {
-      CustomError.throwError(
-        HttpCodes.INTERNAL_SERVER_ERROR,
-        AppCodes.REGISTER_USER_FAILED,
-        'failed to register user'
-      );
+      throw new Error('User registration failed');
     }
   }
 }
